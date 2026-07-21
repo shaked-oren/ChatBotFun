12fe97496af007f3d59d3759ac481954e92d2c72
ענב אורן <shakedororen@gmail.com>
2026-07-21 15:55:03 +0300

    basic front

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
-export default function App() {
-  return <h1>Chat App</h1>
+
+
+function App() {
+
+
+  return (
+    <>
+      <QuestionBar />
+      <MessageOnScreen message="Hello, I am a chatbot. How can I help you today?"/>
+      <MessageOnScreen message="I am a user. I want to know about the weather in Tokyo."/>
+    </>
+  )
 }
+
+function QuestionBar() {
+  const message = "Please enter your question for the chat"
+  return ( 
+    <>
+      <input
+        type="text"
+        placeholder={message}
+      />
+      <button type="submit">Submit</button>
+    </>
+  )
+}
+
+
+function MessageOnScreen(message) {
+  const image = "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+      <img src={image} width="100px" height="100px"/>
+      <p>{message.message}</p>
+    </div>
+  )
+}
+
+export default App
