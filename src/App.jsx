78c5c6d9ba349c5dd7c8d58159ed352a9ee31d48
ענב אורן <shakedororen@gmail.com>
2026-07-28 09:47:47 +0300

    comps: moving input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ChatMessages } from "./components/ChatMessages";
-
+import { MessageInput } from "./components/MessageInput";
 
 function App() {
   const initialMessages = [
@@ -15,50 +15,10 @@ function App() {
   return (
     <div className="app">
       <ChatMessages messageList={messageList} />
-      <QuestionBar messageList={messageList} setMessageList={setMessageList} />
+      <MessageInput messageList={messageList} setMessageList={setMessageList} />
     </div>
   )
 }
-
-function QuestionBar({ messageList, setMessageList }) {
-  const placeholder = "Please enter your question for the chat"
-  const [message, setMessage] = useState("");
-
-  function handleChange(event) {
-    setMessage(event.target.value);
-  }
-
-  function handleSubmit() {
-    const newMessageList = [...messageList, {
-      message: message,
-      sender: "user",
-      id: crypto.randomUUID()
-    }];
-    setMessageList( newMessageList );
-    const newMessageListAfterThinking = [...newMessageList, {
-      message: "Thinking...",
-      sender: "chatbot",
-      id: crypto.randomUUID()
-    }];
-    setMessageList( newMessageListAfterThinking );
-
-    setMessage("");
-  }
-
-  return ( 
-    <div className="question-bar">
-      <input
-        className="question-input"
-        type="text"
-        value={message}
-        placeholder={placeholder}
-        onChange={handleChange}
-      />
-      <button className="submit-button" type="submit" onClick={handleSubmit}>Submit</button>
-    </div>
-  )
-}
-
 
 
 export default App
---
+++ src/components/MessageInput.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function MessageInput({ messageList, setMessageList }) {
+    const placeholder = "Please enter your question for the chat"
+    const [message, setMessage] = useState("");
+  
+    function handleChange(event) {
+      setMessage(event.target.value);
+    }
+  
+    function handleSubmit() {
+      const newMessageList = [...messageList, {
+        message: message,
+        sender: "user",
+        id: crypto.randomUUID()
+      }];
+      setMessageList( newMessageList );
+      const newMessageListAfterThinking = [...newMessageList, {
+        message: "Thinking...",
+        sender: "chatbot",
+        id: crypto.randomUUID()
+      }];
+      setMessageList( newMessageListAfterThinking );
+  
+      setMessage("");
+    }
+  
+    return ( 
+      <div className="question-bar">
+        <input
+          className="question-input"
+          type="text"
+          value={message}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        <button className="submit-button" type="submit" onClick={handleSubmit}>Submit</button>
+      </div>
+    )
+  }
+  
+  
